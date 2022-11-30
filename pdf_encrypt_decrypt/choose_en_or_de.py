98d3b5a9f87fa0c74760874e15c0327ce63d9571
Ruzan <ruzzanv@gmail.com>
2022-12-01 01:14:30 +0400

    run encrypt decrypt

--- pdf_encrypt_decrypt/choose_en_or_de.py
+++ pdf_encrypt_decrypt/choose_en_or_de.py
@@ -4,9 +4,9 @@ from decrypt import decrypt_pdf
 
 def choose_encrypt_decrypt():
     choose = input('Enter 0 to encrypt the file, enter 1 to decrypt: ')
-    if choose == 0:
+    if choose == '0':
         encrypt_pdf()
-    elif choose == 1:
+    elif choose == '1':
         decrypt_pdf()
     else:
         print('Incorrect value')
---
+++ pdf_encrypt_decrypt/run_enc_dec.py
@@ -0,0 +1,4 @@
+from choose_en_or_de import choose_encrypt_decrypt
+
+if __name__ == '__main__':
+    choose_encrypt_decrypt()
